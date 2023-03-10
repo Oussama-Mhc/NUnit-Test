1895b8c96bfd3e209056214d660039f2d4415bf7
Kin <Kin@DESKTOP-JRGLT9T>
2023-03-10 15:32:57 +0100

    Add project files.

---
+++ MyProject.Tests/StringManipulationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Models;
+
+namespace MyProject.Tests
+{
+    [TestFixture]
+    public class StringManipulationTests
+    {
+        [Test]
+        public void Substitute_ReplacesSubstring()
+        {
+            // Arrange
+            string input = "The quick brown fox jumps over the lazy dog.";
+            string search = "quick";
+            string replace = "slow";
+            string expected = "The slow brown fox jumps over the lazy dog.";
+            string actual;
+
+            // Act
+            actual = StringManipulation.Substitute(input, search, replace);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ UnitTest/Models/StringManipulation.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTest.Models
+{
+    public static class StringManipulation
+    {
+        public static string Substitute(string input, string search, string replace)
+        {
+            return input.Replace(search, replace);
+        }
+    }
+}
